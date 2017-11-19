a1c1351135881d2f948035bd4d1ebe7cc6272698
Benedikt Constantin Radtke <benediktradtke@mailbox.tu-berlin.de>
2017-11-19 19:58:48 +0100

    fail if tests fail

--- MyGet.bat
+++ MyGet.bat
@@ -2,4 +2,5 @@ dotnet clean
 dotnet restore
 dotnet build
 dotnet test libsignal-protocol-dotnet-tests\libsignal-protocol-dotnet-tests.csproj
+if not "%errorlevel%"=="0" exit 1
 dotnet pack --include-symbols --include-source
