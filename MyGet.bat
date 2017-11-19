631d7dc1aba1d8c17ba16b48735ae4e400019180
Benedikt Constantin Radtke <benediktradtke@mailbox.tu-berlin.de>
2017-11-19 22:59:46 +0100

    sample nuget push to myget

--- MyGet.bat
+++ MyGet.bat
@@ -4,3 +4,4 @@ dotnet build
 dotnet test libsignal-protocol-dotnet-tests\libsignal-protocol-dotnet-tests.csproj
 if not "%errorlevel%"=="0" exit 1
 dotnet pack --include-symbols --include-source
+rem nuget push libsignal-protocol-dotnet\bin\Debug\libsignal-protocol-dotnet.2.5.3.2.nupkg -SymbolSource https://www.myget.org/F/signal-csharp/symbols/api/v2/packag
